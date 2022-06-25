f9e2942d2a7ad5d5a41c3f8139c60a300cd14d9d
FrancoPiedrabuena <franconpiedrabuena@gmail.com>
2022-06-25 14:04:37 +0000

    mi generador de cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.getElementById("number").innerHTML = randomNumber;
+  document.getElementById("icono").innerHTML = randomSymbol;
+  document.getElementById("Segundoicono").innerHTML = randomSymbol;
 };
+
+let numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+let symbol = ["♦", "♥", "♠", "♣"];
+let randomSymbol = symbol[Math.floor(Math.random() * symbol.length)];
+
+if (randomSymbol == "♥" || randomSymbol == "♦") {
+  document.getElementById("icono").style.color = "red";
+  document.getElementById("Segundoicono").style.color = "red";
+} else {
+  document.getElementById("icono").style.color = "black";
+  document.getElementById("Segundoicono").style.color = "black";
+}
